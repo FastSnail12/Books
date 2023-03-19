75596db28a0c1beeb946d121f7af96453eba0c0a
FastSnail <ytdpkjvftimff2.0.0.0@gmail.com>
2023-03-19 16:39:26 +0300

    job

--- b_search.py
+++ b_search.py
@@ -1,10 +1,9 @@
+from sys import argv
 from typing import Optional
 
 from func_test import check_memory, timer
 
 
-@check_memory
-@timer
 def binary_search(arr: list, item) -> Optional[int]:
     """
     Бинарный поиск. O(lig(n))
@@ -13,11 +12,13 @@ def binary_search(arr: list, item) -> Optional[int]:
     low = 0
     high = len(arr) - 1
 
+    counter = 0
     while low <= high:
         mid = (low + high) // 2
         target = arr[mid]
-
+        counter += 1
         if target == item:
+            print(f"Выполнено итераций: {counter}")
             return mid
         elif target > item:
             high = mid - 1
@@ -26,5 +27,15 @@ def binary_search(arr: list, item) -> Optional[int]:
     return None
 
 
+@check_memory
+@timer
+def test(len_arr: int):
+    arr = []
+    for i in range(len_arr):
+        arr.append(i)
+    binary_search(arr, len_arr // 2 + 1)
+
+
 if __name__ == "__main__":
-    z = binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)
+    len_array = argv[-1]
+    test(int(len_array))
