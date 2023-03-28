cfbdc4b3eb6da1ccd61991f2e09e8858518095d9
FastSnail <ytdpkjvftimff2.0.0.0@gmail.com>
2023-03-28 16:12:34 +0300

    job

--- sql/SQL_quickstart_guide/sql_stunes_test.sql
+++ sql/SQL_quickstart_guide/sql_stunes_test.sql
@@ -124,3 +124,154 @@ WHERE
   Total = 1.98
 ORDER BY
   InvoiceDate;
+
+/*
+ * CREATED BY: Snail
+ * DESCRIPTION: Рассмотрим следующий запрос, который возвращает счета,
+ * находящиеся в диапазоне от $1,98 до $5,00.
+*/
+
+SELECT
+  InvoiceDate,
+  BillingAddress,
+  BillingCity,
+  Total
+FROM 
+  invoices
+WHERE
+  Total BETWEEN 1.98 AND 5.00 --Total >= 1.98 AND Total <= 5.00
+ORDER BY
+  InvoiceDate;
+
+/*
+ * CREATED BY: Snail
+ * DESCRIPTION: Следующий запрос возвращает только суммы
+ * счетов-фактур, равные $1,98 или $3,96.
+*/
+
+SELECT
+  InvoiceDate,
+  BillingAddress,
+  BillingCity,
+  Total
+FROM 
+  invoices
+WHERE
+  Total IN (1.98, 3.96)
+ORDER BY
+  InvoiceDate;
+
+/*
+ * CREATED BY: Snail
+ * DESCRIPTION: Следующий запрос возвращает все счета,
+ * выставленные в городе Тусон.
+*/
+
+SELECT
+  InvoiceDate,
+  BillingAddress,
+  BillingCity,
+  Total
+FROM 
+  invoices
+WHERE
+  BillingCity = 'Tucson'
+ORDER BY
+  Total;
+
+/*
+ * CREATED BY: Snail
+ * DESCRIPTION: Запрос ищет любые счета, выставленные
+ * в городах на букву T. В результат теперь попали
+ * Торонто и Тусон.
+*/
+
+SELECT
+  InvoiceDate,
+  BillingAddress,
+  BillingCity,
+  Total
+FROM 
+  invoices
+WHERE
+  BillingCity LIKE 'T%'
+ORDER BY
+  Total;
+
+/*
+ * CREATED BY: Snail
+ * DESCRIPTION: Добавление еще одного символа % перед
+ * буквой T изменит условие поиска на поиск счета,
+ * выставленного в городе, название которого содержит
+ * букву T.
+*/
+
+SELECT
+  InvoiceDate,
+  BillingAddress,
+  BillingCity,
+  Total
+FROM 
+  invoices
+WHERE
+  BillingCity LIKE '%T%'
+ORDER BY
+  Total;
+
+/*
+ * CREATED BY: Snail
+ * DESCRIPTION: Оператор LIKE также используется для
+ * исключения результатов, соответствующих указанным
+ * параметрам. Для этого необходимо поставить ключевое
+ * слово NOT перед оператором LIKE, и вы сможете
+ * исключить записи из результата запроса.
+*/
+
+SELECT
+  InvoiceDate,
+  BillingAddress,
+  BillingCity,
+  Total
+FROM 
+  invoices
+WHERE
+  BillingCity NOT LIKE '%T%'
+ORDER BY
+  Total;
+
+/*
+ * CREATED BY: Snail
+ * DESCRIPTION: Используя все знания как о числах,
+ * так и о тексте, вы сможете теперь выполнить поиск
+ * счета, выставленного в определенную дату.
+*/
+
+SELECT
+  InvoiceDate,
+  BillingAddress,
+  BillingCity,
+  Total
+FROM 
+  invoices
+WHERE
+  InvoiceDate = '2009-01-03 00:00:00'
+ORDER BY
+  Total DESC;
+
+/*
+ * CREATED BY: Snail
+ * DESCRIPTION: Функция DATE() позволяет исключить
+ * время при указании параметров даты.
+*/
+
+SELECT
+  InvoiceDate,
+  BillingAddress,
+  BillingCity,
+  Total
+FROM 
+  invoices
+WHERE
+  DATE(InvoiceDate) = '2009-01-03'
+ORDER BY
+  Total DESC;
